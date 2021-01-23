a6e7c3c580d2a0a9e211283aabfa5614f12fe38d
MURAT ÇOBAN <murcoban@gmail.com>
2021-01-23 16:24:50 +0300

    Proje dosyası ekle.

---
+++ Dictionary/Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    //U:Ürün
+    //A:Adet
+    class Dictionary<U>  //Generic Class
+    {
+        U[] items;
+
+        // class new olunca MyList otomatik çalışır
+        public Dictionary()
+        {
+            items = new U[0];
+           }
+
+        public void Add(U item)
+        {
+            U[] tempItem = items;
+            items = new U[items.Length + 1];
+            for (int i = 0; i < tempItem.Length; i++)
+            {
+                items[i] = tempItem[i];
+            }
+            items[items.Length - 1] = item;
+
+        }
+        public U[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string> urunler = new Dictionary<string>();
+            urunler.Add("BİLGİSAYAR");
+            urunler.Add("SES KARTI");
+
+            foreach (var item in urunler.Items)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
